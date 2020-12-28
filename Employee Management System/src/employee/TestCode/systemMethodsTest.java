d64c5ae3f90c1c9b5521d3119d6b69afb21ce7f6
afraAntara <76681973+afraAntara@users.noreply.github.com>
2020-12-28 15:17:01 +0600

    Add files via upload

---
+++ Employee Management System/src/employee/TestCode/systemMethodsTest.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import employee.systemMethods;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static java.util.Collections.list;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User
+ */
+public class systemMethodsTest {
+    
+    systemMethods m;
+   
+    
+    @Before
+    public void setUp() throws Exception {
+        m = new systemMethods();
+    }
+      
+    
+    @Test
+    public void testLogin(){
+        String u = "afra";
+        String p = "afra";
+        
+        boolean b = m.login(u, p);
+        assertTrue(b);
+        
+    }
+    
+    @Test
+    public void testAddEmployee() {
+      assertEquals(Arrays.asList("1", "Afra", "20","2","John","22","3","Tom","24"), m.AddEmployee());
+    }
+    
+    @Test
+    public void testRemoveEmployees(){
+     assertEquals(Arrays.asList(), m.RemoveEmployee());
+    }
+    
+    @Test
+    public void testUpdateEmployee(){
+        
+      assertEquals("Anna",m.UpdateEmployee());    
+    
+    }
+   
+    @Test
+    public void testSearchEmployee(){
+     
+       assertEquals(Arrays.asList("1","Afra", "20","2","John","22","3","Tom","24").contains("Afra"), m.SearchEmployee());
+        
+    }
+    
+    @Test
+    public void testViewDetails(){   
+        assertEquals("You can now view the details",m.ViewDetails());   
+    }
+     
+    
+    @Test
+    public void testForgotPassword(){
+        String old_password = "afra";
+        String new_password = "admin";
+        
+        assertEquals("New Password Set Successfully",m.ForgotPassword(old_password, new_password));
+    }
+    
+    
+    @Test
+    public void testRequestForSalary() {
+        
+       assertEquals("Approved",m.salary(500));
+    }
+    
+    
+    @Test
+    public void testOvertime(){
+        
+        assertEquals(2700,m.Overtime(2500, 4, 50));  
+    
+    }
+    
+    @Test
+    public void testTax(){
+   
+      assertEquals(225, m.Tax(1500, 0.15),0);
+      
+    }
+    
+    }
+    
+    
+   
+    
+    
+    
+
+
